4c5dd501b5fa7a9fb1bfbfbb0af5aaea59e8df3f
MobinaEbrahimi <mobina.pourmehdi85@gmail.com>
2025-03-04 14:14:00 +0330

    make st

---
+++ student.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Afra on 3/4/2025.
+//
+
+#include "student.h"
---
+++ student.h
@@ -0,0 +1,14 @@
+//
+// Created by Afra on 3/4/2025.
+//
+
+#ifndef UNTITLED21_STUDENT_H
+#define UNTITLED21_STUDENT_H
+
+
+class student {
+
+};
+
+
+#endif //UNTITLED21_STUDENT_H
